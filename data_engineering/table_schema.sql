b1492d22aabf374d46fb8d5ce438421321d47c82
DavidMoon-184 <53849210+DavidMoon-184@users.noreply.github.com>
2021-06-09 22:57:41 -0700

    Add files via upload

---
+++ data_engineering/table_schema.sql
@@ -0,0 +1,60 @@
+--Drop tables if exists
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS titles;
+
+
+-- Create tables 
+CREATE TABLE departments (
+	dept_no VARCHAR NOT NULL,
+	dept_name VARCHAR NOT NULL,
+	PRIMARY KEY (dept_no),
+);
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+CREATE TABLE employees (
+	emp_no INT NOT NULL,
+    emp_title_id INT NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	gender VARCHAR NOT NULL,
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no)
+    FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
+);
+CREATE TABLE managers (
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no)
+);
+CREATE TABLE titles (
+    title_id VARCHAR,
+	title VARCHAR NOT NULL,
+    PRIMARY KEY (title_id),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+-- Check tables
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM employees;
+SELECT * FROM managers;
+SELECT * FROM salaries;
+SELECT * FROM titles;
